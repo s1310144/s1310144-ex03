e4db0f1ed8017453cc0f78b7843a91bca8ed4ef2
s1310144 <s1310144@u-aizu.ac.jp>
2024-07-04 01:24:14 +0900

    User name added

--- game.c
+++ game.c
@@ -4,11 +4,16 @@
 
 int main(void){
   int i, h=0, t=0;
+  char name[100];
+
+  printf("Who are you?\n> ");
+  scanf("%s", name);
+  printf("Hello, %s\n", name);
+
   srand((unsigned int) time(NULL));
-  
   for(i=0;i<3;i++){
     printf("Round %d: ", i+1);
-    if(rand() == 1){
+    if(rand()%2 == 1){
       printf("Heads\n");
       h++;
     }
