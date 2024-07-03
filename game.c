14b8c8ce958ffd84f61e171403af5cbc8c137b53
s1310144 <s1310144@u-aizu.ac.jp>
2024-07-04 02:01:35 +0900

    Final

--- game.c
+++ game.c
@@ -4,6 +4,13 @@
 
 int main(void){
   int i, h=0, t=0;
+  char name[100];
+
+  printf("Who are you?\n> ");
+  scanf("%s", name);
+  printf("Hello, %s!\n", name);
+
+  printf("Tossing a coin...\n");
   
   srand((unsigned int) time(NULL));
   for(i=0;i<3;i++){
@@ -19,6 +26,8 @@ int main(void){
   }
 
   printf("Heads: %d, Tails: %d\n", h, t);
+  if(h>t) printf("%s won!\n", name);
+  else printf("%s lost!\n", name);
   
   return 0;
 }
