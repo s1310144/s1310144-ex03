95a637fdabaa2d50a9c7a717523584efaab675b5
s1310144 <s1310144@u-aizu.ac.jp>
2024-07-04 00:23:55 +0900

    HoT: initial revision

---
+++ game.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(void){
+  int i, h=0, t=0;
+  srand((unsigned int) time(NULL));
+  
+  for(i=0;i<3;i++){
+    printf("Round %d: ", i+1);
+    if(rand() == 1){
+      printf("Heads\n");
+      h++;
+    }
+    else{
+      printf("Tails\n");
+      t++;
+    }
+  }
+
+  printf("Heads: %d, Tails: %d\n", h, t);
+  
+  return 0;
+}
